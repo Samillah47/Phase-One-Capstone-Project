8080a880a5f7b7341424195cb88a879b5cbbd562
Samillah MUTONI <samillah.mutoni@gmail.com>
2026-02-23 09:54:37 +0200

    Exceptions

---
+++ src/exceptions/CourseFullException.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+public class CourseFullException extends Exception {
+    
+    private String courseId;
+    private String courseName;
+    private int maxCapacity;
+
+    public CourseFullException(String courseId, String courseName, int maxCapacity) {
+        super(String.format(
+            "Course '%s' (%s) is FULL! Maximum capacity of %d students reached. " +
+            "Please try another section or add yourself to the waitlist.",
+            courseName, courseId, maxCapacity
+        ));
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.maxCapacity = maxCapacity;
+    }
+
+    public CourseFullException(String message) {
+        super(message);
+    }
+
+    public String getCourseId() { return courseId; }
+    public String getCourseName() { return courseName; }
+    public int getMaxCapacity() { return maxCapacity; }
+}
---
+++ src/exceptions/StudentAlreadyEnrolledException.java
@@ -0,0 +1,31 @@
+package exceptions;
+
+public class StudentAlreadyEnrolledException extends Exception {
+
+    private String studentId;
+    private String studentName;
+    private String courseId;
+    private String courseName;
+
+    public StudentAlreadyEnrolledException(String studentId, String studentName,
+                                            String courseId, String courseName) {
+        super(String.format(
+            "Student '%s' (ID: %s) is ALREADY enrolled in '%s' (%s). " +
+            "Duplicate enrollment is not permitted.",
+            studentName, studentId, courseName, courseId
+        ));
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.courseId = courseId;
+        this.courseName = courseName;
+    }
+
+    public StudentAlreadyEnrolledException(String message) {
+        super(message);
+    }
+
+    public String getStudentId() { return studentId; }
+    public String getStudentName() { return studentName; }
+    public String getCourseId() { return courseId; }
+    public String getCourseName() { return courseName; }
+}
